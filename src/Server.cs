c1c57fdf2e421fdc9adba4919c2f77e2601e7876
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-04-08 12:49:46 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,12 +2,21 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Console.WriteLine("Logs from your program will appear here!");
-
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var acceptedSocket = server.AcceptSocket();
 
-string response = "HTTP/1.1 200 OK\r\n\r\n";
-var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
-Console.WriteLine(r);
+var acceptedSocket = server.AcceptSocket();
+byte[] recived_message = new byte[256];
+var bytes_recived = acceptedSocket.Receive(recived_message);
+var recived = Encoding.UTF8.GetString(recived_message);
+Console.WriteLine(recived);
+
+if (recived.Contains("GET / HTTP/1.1")){
+    string response = "HTTP/1.1 200 OK\r\n\r\n";
+    var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
+    Console.WriteLine(r);
+} else {
+    string response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
+    Console.WriteLine(r);
+}
