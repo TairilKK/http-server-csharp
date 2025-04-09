b0b2367038ae4e9fb6d473345602ed962c7aa431
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-04-09 11:07:14 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
@@ -8,14 +9,23 @@ server.Start();
 var acceptedSocket = server.AcceptSocket();
 byte[] recived_message = new byte[256];
 var bytes_recived = acceptedSocket.Receive(recived_message);
-var recived = Encoding.UTF8.GetString(recived_message);
-Console.WriteLine(recived);
-
-if (recived.Contains("GET / HTTP/1.1")){
+var received = Encoding.UTF8.GetString(recived_message);
+Console.WriteLine(received);
+if (System.Text.RegularExpressions.Regex.IsMatch(received, @"^GET \/ HTTP\/1\.1")){
     string response = "HTTP/1.1 200 OK\r\n\r\n";
     var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
     Console.WriteLine(r);
-} else {
+}
+else if (System.Text.RegularExpressions.Regex.IsMatch(received, @"^GET \/echo\/[^ ]+ HTTP\/1\.1"))
+{
+    var match = System.Text.RegularExpressions.Regex.Match(received, @"^GET \/echo\/([^ ]+) HTTP\/1\.1");
+    string content = match.Groups[1].Value;
+
+    string response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}";
+    var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
+    Console.WriteLine(r);
+}
+else {
     string response = "HTTP/1.1 404 Not Found\r\n\r\n";
     var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
     Console.WriteLine(r);
