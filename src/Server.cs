f500534cec0a4bc9b72214966d32586b039d7ad3
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-04-21 10:19:44 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,10 +2,8 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
-// using System.Threading;
 
 string? directory = null;
-
 for (int i = 0; i < args.Length; i++)
 {
     if (args[i] == "--directory" && i + 1 < args.Length)
@@ -24,8 +22,9 @@ void handleSocket(Socket acceptedSocket) {
     var received = Encoding.UTF8.GetString(recived_message);
     Console.WriteLine($"Recived: {received}");
     if (Regex.IsMatch(received, @"^GET \/ HTTP\/1\.1")){
-        string response = "HTTP/1.1 200 OK\r\n\r\n";
-        var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
+        ResponseBuilder rb = new ResponseBuilder();
+        rb.SetStatus(200);
+        var r = acceptedSocket.Send(rb.Create());
         Console.WriteLine(r);
     }
     else if (Regex.IsMatch(received, @"^GET \/echo\/[^ ]+ HTTP\/1\.1"))
@@ -61,8 +60,9 @@ void handleSocket(Socket acceptedSocket) {
         var filePath = $"{directory}/{match.Groups[1].Value}";
         Console.WriteLine(filePath);
         if(!File.Exists(filePath)){
-            string responsea = "HTTP/1.1 404 Not Found\r\n\r\n";
-            var ra = acceptedSocket.Send(Encoding.UTF8.GetBytes(responsea));
+            ResponseBuilder rb = new ResponseBuilder();
+            rb.SetStatus(404);
+            var ra = acceptedSocket.Send(rb.Create());
             Console.WriteLine(ra);
             return;
         }
@@ -75,9 +75,80 @@ void handleSocket(Socket acceptedSocket) {
         var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
         Console.WriteLine(r);
     }
+    else if (Regex.IsMatch(received, @"^POST \/files\/[^ ]+ HTTP\/1\.1"))
+    {
+        var match = Regex.Match(received,
+            @"^(?<method>\w+)\s(?<path>[^\s]+)\sHTTP/1\.1\r?\n" +
+            @"Host:\s(?<host>[^\r\n]+)\r?\n" +
+            @"Content-Length:\s(?<contentLength>\d+)\r?\n" +
+            @"Content-Type:\s(?<contentType>[^\r\n]+)\r?\n\r?\n" +
+            @"(?<body>.*)$",
+            RegexOptions.Singleline);
+
+        if (match.Success)
+        {
+            string method = match.Groups["method"].Value;
+            string path = match.Groups["path"].Value;
+            string host = match.Groups["host"].Value;
+            int contentLength = int.Parse(match.Groups["contentLength"].Value);
+            string contentType = match.Groups["contentType"].Value;
+            string body = match.Groups["body"].Value;
+
+            Console.WriteLine($"Method: {method}");
+            Console.WriteLine($"Path: {path}");
+            Console.WriteLine($"Host: {host}");
+            Console.WriteLine($"Content-Length: {contentLength}");
+            Console.WriteLine($"Content-Type: {contentType}");
+            Console.WriteLine($"Body: {body}");
+
+            try
+            {
+
+                string sanitizedPath = Path.GetFileName(path);
+                if (string.IsNullOrEmpty(sanitizedPath))
+                {
+                    throw new ArgumentException("Invalid file name");
+                }
+
+                Directory.CreateDirectory(directory);
+
+                string fullPath = Path.Combine(directory, sanitizedPath);
+
+                File.WriteAllText(fullPath, body.Substring(0, contentLength));
+
+                ResponseBuilder rb = new ResponseBuilder();
+                rb.SetStatus(ResponseCodes.CREATED);
+                var r = acceptedSocket.Send(rb.Create());
+                Console.WriteLine(r);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                ResponseBuilder rb = new ResponseBuilder();
+                rb.SetStatus(ResponseCodes.FORBIDDEN);
+                var r = acceptedSocket.Send(rb.Create());
+                Console.WriteLine(r);
+            }
+            catch (IOException ex)
+            {
+                ResponseBuilder rb = new ResponseBuilder();
+                rb.SetStatus(ResponseCodes.INTERNAL_SERVER_ERROR);
+                var r = acceptedSocket.Send(rb.Create());
+                Console.WriteLine(r);
+            }
+        }
+        else{
+            ResponseBuilder rb = new ResponseBuilder();
+            rb.SetStatus(404);
+            var r = acceptedSocket.Send(rb.Create());
+            Console.WriteLine(r);
+        }
+
+    }
     else {
-        string response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        var r = acceptedSocket.Send(Encoding.UTF8.GetBytes(response));
+        ResponseBuilder rb = new ResponseBuilder();
+        rb.SetStatus(404);
+        var r = acceptedSocket.Send(rb.Create());
+        Console.WriteLine(r);
     }
 }
 
